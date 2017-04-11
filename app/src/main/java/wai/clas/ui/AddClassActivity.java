8daad7091fdb69befdff17bab0d395e0a3e75127
1031066280@qq.com <chang19930228>
2017-04-11 18:00:52 +0800

    新加Afinal框架，新添2个自定义控件

--- app/src/main/java/wai/clas/ui/AddClassActivity.java
+++ app/src/main/java/wai/clas/ui/AddClassActivity.java
@@ -1,5 +1,6 @@
 package wai.clas.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
@@ -9,10 +10,16 @@ import android.widget.LinearLayout;
 
 import net.tsz.afinal.view.TitleBar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.bmob.v3.datatype.BmobFile;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.SaveListener;
+import cn.bmob.v3.listener.UploadBatchListener;
+import me.iwf.photopicker.PhotoPicker;
 import wai.clas.base.BaseActivity;
 import wai.clas.method.Utils;
 import wai.clas.model.AskManage;
@@ -69,6 +76,69 @@ public class AddClassActivity extends BaseActivity {
 //                });
             }
         });
+        class1Iv.setOnClickListener(view -> PhotoPicker.builder()
+                .setPhotoCount(3)
+                .setShowCamera(true)
+                .setShowGif(true)
+                .setPreviewEnabled(false)
+                .start(this, PhotoPicker.REQUEST_CODE));
+        class2Iv.setOnClickListener(view -> PhotoPicker.builder()
+                .setPhotoCount(2)
+                .setShowCamera(true)
+                .setShowGif(true)
+                .setPreviewEnabled(false)
+                .start(this, PhotoPicker.REQUEST_CODE));
+        class3Iv.setOnClickListener(view -> PhotoPicker.builder()
+                .setPhotoCount(1)
+                .setShowCamera(true)
+                .setShowGif(true)
+                .setPreviewEnabled(false)
+                .start(this, PhotoPicker.REQUEST_CODE));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PhotoPicker.REQUEST_CODE) {
+            if (data != null) {
+                ArrayList<String> photos =
+                        data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
+                final String[] filePaths = new String[photos.size()];
+                for (int i = 0; i < photos.size(); i++) {
+                    filePaths[i] = photos.get(i);
+                }
+                BmobFile.uploadBatch(filePaths, new UploadBatchListener() {
+                    @Override
+                    public void onSuccess(List<BmobFile> files, List<String> urls) {
+                        if (urls.size() == filePaths.length) {//如果数量相等，则代表文件全部上传完成
+                            for (int i = 0; i < files.size(); i++) {
+                                if (i == 0) {
+                                    class1Iv.setImageBitmap(Utils.getBitmapByFile(photos.get(0)));
+                                } else if (i == 1) {
+                                    class2Iv.setImageBitmap(Utils.getBitmapByFile(photos.get(1)));
+                                } else if (i == 2) {
+                                    class3Iv.setImageBitmap(Utils.getBitmapByFile(photos.get(2)));
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onError(int statuscode, String errormsg) {
+
+                    }
+
+                    @Override
+                    public void onProgress(int curIndex, int curPercent, int total, int totalPercent) {
+                        //1、curIndex--表示当前第几个文件正在上传
+                        //2、curPercent--表示当前上传文件的进度值（百分比）
+                        //3、total--表示总的上传文件数
+                        //4、totalPercent--表示总的上传进度（百分比）
+                    }
+                });
+            }
+        }
+
     }
 
     @Override
